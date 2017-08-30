11c7b01055d516ff001d1b1c7da8da918a6d6b22
ivanovishx <ivanlozanox@gmail.com>
2017-08-30 01:32:15 -0700

    receiving cloud, TODO #1 clean code, #2 add an ID to recieve two diferent clouds(ROA and BODYP)

--- ROA_client/ROA_client/ROA_client/ROA_client.cpp
+++ ROA_client/ROA_client/ROA_client/ROA_client.cpp
@@ -13,7 +13,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <vector>
+#include <string>
+#include <stdint.h>
 
 #ifdef WIN32
 //add ws2_32.lib to linker on VC++
@@ -34,14 +36,35 @@
 #define BUFLEN 2048
 #define MSGS 5	/* number of messages to send */
 
+
+typedef struct individualCloud
+{
+	uint32_t index;
+	float x = 0;
+	float y = 0;
+	float z = 0;
+	uint32_t rgba = 0x000000;
+	// uint32_t rgba = 0;
+
+} typeIndividualCloud;
+
 struct sockaddr_in myaddr, remaddr;
 int fd, i, slen = sizeof(remaddr);
 char buf[BUFLEN];	/* message buffer */
 int recvlen;		/* # bytes in acknowledgement message */
 char *server = "127.0.0.1";	/* change this to use a different server */
 
+/*------FUNCTIONS DECLARATION------*/
+// void split( char* s, char c, char* v );
+void split( std::string& s, char c, std::vector<std::string>& v );
+// void split( string s, char c, vector<string> v );
+void set_point_packet(individualCloud* _individualCloud, individualCloud cloud_spliter, uint32_t point_index );
+void split_point_packet(char* bufin, individualCloud  cloud_spliter, uint32_t* point_index  );
 void writeSocket();
-void readSocket();
+char* readSocket();
+
+
+using namespace std;
 
 int main(void)
 {
@@ -86,34 +109,120 @@ int main(void)
 			perror("sendto");
 			exit(1);
 		}
-		/* now receive an acknowledgement from the server */
-		recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &slen);
-		if (recvlen >= 0) {
-			buf[recvlen] = 0;	/* expect a printable string - terminate it */
-			printf("received message: \"%s\"\n", buf);
-		}
+		// 	/* now receive an acknowledgement from the server */
+		// 	recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &slen);
+		// 	if (recvlen >= 0) {
+		// 		buf[recvlen] = 0;	/* expect a printable string - terminate it */
+		// 		printf("received message: \"%s\"\n", buf);
+		// 	}
 	}
 
 
 	//Listen new messages from ROA_SERVER sending individualCloud:
 	printf("\nListening comming Cloud Packets:\n");
 
+	char* bufin = new char[BUFLEN];
+	int siezeMsg = 0;
+	//------POINT DATA PACKET VARIABLES------
+	struct individualCloud* ROA_individualCloud = new individualCloud[307200];
+	struct individualCloud cloud_spliter;
+	uint32_t point_index = 0;
+
+//wwwww
+	// memset (&ROA_individualCloud, 0, sizeof (ROA_individualCloud));//reset frame
+
 	while (1) {
 
-		// writeSocket();
-		readSocket();
+
+		bufin = readSocket();
+
+		if (bufin[0] == '$') {
+			if (bufin[1] == 'S' && bufin[2] == 'T' && bufin[3] == 'A') {
+				printf("START!!!\n");
+			}
+			else if ( bufin[1] == 'E' && bufin[2] == 'N' && bufin[3] == 'D') {
+				printf("END!!!\n");
+			}
+			else {
+
+				split_point_packet( bufin,  cloud_spliter, &point_index);
+				 set_point_packet(ROA_individualCloud, cloud_spliter, point_index);
+				 //**/printf("index: %u, color: %u \n", point_index, cloud_spliter.rgba);
+
+				// printf("index:%u %f %f %u \n", ROA_individualCloud[300].index, ROA_individualCloud[300].x, ROA_individualCloud[300].y, ROA_individualCloud[300].rgba );
+				 // printf("index:%u %f %f %u \n", ROA_individualCloud[20].index, ROA_individualCloud[20].x, ROA_individualCloud[20].y, ROA_individualCloud[20].rgba );
+			}
+		}
+		// printf("--Value: %s\n", bufin);
 
 	}
+	// writeSocket();
 	//END
 	WSACleanup(); //call this for destructor
 	// close(fd);
 	return 0;
 }
 
+ void set_point_packet(individualCloud* _individualCloud, individualCloud cloud_spliter, uint32_t point_index  ) {
+
+ 	_individualCloud[point_index].index = point_index;
+ 	_individualCloud[point_index].x =  cloud_spliter.x;
+ 	_individualCloud[point_index].y = cloud_spliter.y;
+ 	_individualCloud[point_index].z = cloud_spliter.z;
+ 	_individualCloud[point_index].rgba = cloud_spliter.rgba;
+
+
+ 	/*point_index = (uint32_t)20;
+ 	_individualCloud[point_index].index = point_index+1;
+ 	_individualCloud[point_index].x =  3;
+ 	_individualCloud[point_index].y = 45;
+ 	_individualCloud[point_index].z = cloud_spliter.z;
+ 	_individualCloud[point_index].rgba = 0xA185FFFF;*/
+
+ }
+
+
+void split( std::string& s, char c, std::vector<std::string>& v) {
+	string::size_type i = 0;
+	string::size_type j = s.find(c);
+
+	while (j != string::npos) {
+		v.push_back(s.substr(i, j - i));
+		i = ++j;
+		j = s.find(c, j);
+
+		if (j == string::npos)
+			v.push_back(s.substr(i, s.length()));
+	}
+}
+
+void split_point_packet(char* bufin, individualCloud cloud_spliter,  uint32_t* point_index  ) {
+
+	std::string string_vector (sizeof(typeIndividualCloud), 0);
+
+	string_vector = std::string(bufin);
+
+	std::vector<std::string> v;
+	split(string_vector, ',', v);
+
+
+	// printf("myvector stores:%d\n", int(v.size()) );
+	// cloud_spliter.index = stoul(v[0]);
+	cloud_spliter.index = std::stoul(v[1]);
+	cloud_spliter.x = std::stof(v[2]);
+	cloud_spliter.y = std::stof(v[3]);
+	cloud_spliter.z = std::stof(v[4]);
+	cloud_spliter.rgba = std::stoul(v[5]);
+	// printf("index:%u %f %f %u \n", cloud_spliter.index, cloud_spliter.x, cloud_spliter.y, cloud_spliter.rgba );
+
+	*point_index = cloud_spliter.index;
+
+
+}
 
 void writeSocket() {
 
-	i=9;
+	i = 9;
 	printf("Sending packet %d to %s port %d\n", i, server, SERVICE_PORT);
 	sprintf(buf, "This is packet %d CODE:0xA185FFFF", i);
 	if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen) == -1) {
@@ -124,12 +233,17 @@ void writeSocket() {
 }
 
 
-void readSocket() {
+char* readSocket() {
+
+
+	char* bufin = new char[BUFLEN];
+	memset (&bufin, 0, sizeof (bufin));
 	recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &slen);
 	if (recvlen >= 0) {
 		buf[recvlen] = 0;	/* expect a printable string - terminate it */
-		printf("received message: \"%s\"\n", buf);
+		bufin = buf;
+		// printf("received message: \"%s\"\n", buf);
 	}
-
-
+	// else sprintf (bufin, "--ERROR IN DATA--\n");
+	return bufin;
 }
