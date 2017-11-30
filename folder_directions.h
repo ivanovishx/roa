b09ddb0bdcdb6aab3dd9aaa3ec0be4acb092e78c
Ivanovish <ivanlozanox@gmail.com>
2017-11-29 20:26:37 -0800

    Create folder_directions.h

---
+++ folder_directions.h
@@ -0,0 +1,4 @@
+C:\Users\Ivan\Documents\tesis\rs\src
+C:\Users\Ivan\Documents\tesis\rs\build\Debug
+C:\Users\Ivan\Documents\tesis\roa\ROA_CLIENT_VIEWPCL_V2\ROA_CLIENT_VIEWPCL_V2\ROA_CLIENT_VIEWPCL_V2
+C:\Users\Ivan\Documents\tesis\roa\ROA_CLIENT_VIEWPCL_V2\ROA_CLIENT_VIEWPCL_V2\x64\Debug
