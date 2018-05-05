697ab4cbdf9c30d9da11a27bde29ea382d93ce3b
Ivanovish <ivanlozanox@gmail.com>
2018-05-05 09:32:09 -0700

    Update serverROA.cpp

--- rs/src/serverROA.cpp
+++ rs/src/serverROA.cpp
@@ -26,7 +26,7 @@
 #undef DEBUG_PRINT
 
 
-long serverROA::now_ms() {
+uint64_t serverROA::now_ms() {
 
   ftime(&start);
   long timestamp = clock();
@@ -35,6 +35,9 @@ long serverROA::now_ms() {
 
 //ssssssss
 bool serverROA::sendTCPpack(char* buffer) {
+  printf("----------BUFFIN:\n");
+  printf(buffer);
+  printf("\n");
   if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&remaddr, addrlen) < 0) {
     perror("sendto error?");
     return 0;
@@ -46,14 +49,16 @@ bool serverROA::sendTCPpack(char* buffer) {
 
 bool serverROA::send_start_pkt(int ID) {
   char* buffer = new char[BUFSIZE];
-  sprintf (buffer, "$START,%d;", ID);
+  sprintf (buffer, "$START,%d;TM: %u", ID, now_ms());
+  // sprintf (buffer, "$START,%d;", ID);
   printf("Sending message: \"%s\"\n", buffer);
   return sendTCPpack(buffer);
 }
 
 bool serverROA::send_end_pkt(int ID) {
   char* buffer = new char[BUFSIZE];
-  sprintf (buffer, "$END,%d;", ID);
+  sprintf (buffer, "$END,%d;TM: %u", ID, now_ms());
+  // sprintf (buffer, "$END,%d;", ID);
   printf("Sending message: \"%s\"\n", buffer);
   return sendTCPpack(buffer);
 }
@@ -63,7 +68,7 @@ bool serverROA::get_start_confirmation() {
   recvlen = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
   if (recvlen > 0) {
     buffer[recvlen] = 0;
-    printf("received message: \"%s\" (%d bytes)\n", buffer, recvlen);
+    printf("Waiting for START COFIRMATION:received message: \"%s\" (%d bytes)\n", buffer, recvlen);
     if (buffer[0] == '$' && buffer[1] == 's' && buffer[2] == 't' && buffer[3] == 'a') {
       return 0;
     }
@@ -78,7 +83,7 @@ bool serverROA::get_end_confirmation() {
   recvlen = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
   if (recvlen > 0) {
     buffer[recvlen] = 0;
-    printf("received message: \"%s\" (%d bytes)\n", buffer, recvlen);
+    printf("Waiting for END CONFIRMATION:received message: \"%s\" (%d bytes)\n", buffer, recvlen);
     if (buffer[0] == '$' && buffer[1] == 'e' && buffer[2] == 'n' && buffer[3] == 'd') {
       return 0;
     }
@@ -91,10 +96,14 @@ bool serverROA::get_end_confirmation() {
 bool serverROA::get_point_confirmation(int index_comp) {
   char* buffer = new char[BUFSIZE];
   recvlen = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+  // printf(".c");
   if (recvlen > 0) {
     buffer[recvlen] = 0;
     // printf("---Confirmation Point: \"%s\" (%d bytes)\n", buffer, recvlen);
-    // printf("Comparing: %d ::AND:: %d\n", std::atoi(buffer), index_comp);
+    if (now_ms() - timerCondition > 1000) {
+      // printf(buf);
+      // printf("Comparing: LO:%d ::AND:: RE:%d\n", index_comp, std::atoi(buffer));
+    }
     if (std::atoi(buffer) == index_comp) {
       // if(0){
       return 0;
@@ -107,7 +116,8 @@ bool serverROA::get_point_confirmation(int index_comp) {
 
 bool serverROA::sendIndividualPoint(struct individualCloud* sendCloud,  uint32_t index) {
   char* buffer = new char[BUFSIZE];
-  sprintf (buffer, "$,%u,%f,%f,%f,%u;", (uint32_t)sendCloud[index].index, sendCloud[index].x, (float)sendCloud[index].y, (float)sendCloud[index].z, sendCloud[index].rgba);
+  sprintf (buffer, "$PNT,%u,%f,%f,%f,%u;", (uint32_t)sendCloud[index].index, sendCloud[index].x, (float)sendCloud[index].y, (float)sendCloud[index].z, sendCloud[index].rgba);
+  // printf(buffer);
   if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&remaddr, addrlen) < 0) {
     perror("sendto error?");
     return 0;
@@ -120,13 +130,14 @@ serverROA::serverROA() {
   //ScheduleTask* taskHandeler = new ScheduleTask();
   initialize_connection();
   listener();
+  timerCondition = now_ms();
 }
 
 void serverROA::listener() {
   /* now loop, receiving data and printing what we received */
   char* buffer = new char[BUFSIZE]; /* receive buffer */
   /* now loop, receiving data and printing what we received */
-
+  // for (int i = 0; i == 4; i++) {
   for (int i = 0; i < 4; i++) {
     printf("waiting on port %d\n", ROA_TX_PORT);
     recvlen = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
